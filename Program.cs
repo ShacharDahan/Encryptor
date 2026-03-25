d677bd16af64c406a7d2dc1a9847c238553433f1
Shachar Dahan <Shachard56@gmail.com>
2026-03-25 23:26:14 +0200

    Last PR fix

--- Program.cs
+++ Program.cs
@@ -1,32 +1,10 @@
-﻿using Encryptor.Managers;
-
-namespace Encryptor
+﻿namespace Encryptor
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var encryptionService = new EncryptionService();
-
-            switch (UserInputManager.GetUserSelection())
-            {
-                case UserOptions.Encrypt:
-                    encryptionService.Encrypt();
-                    break;
-                case UserOptions.Decrypt:
-                    encryptionService.Decrypt();
-                    break;
-                case UserOptions.Exit:
-                    IOManager.WriteLine("bye bye");
-                    Environment.Exit(0);
-                    break;
-                default:
-                    IOManager.WriteLine("How did you even get here????");
-                    break;
-            }
-
-            IOManager.WriteLine("Press key to exit...");
-            IOManager.ReadLine();
+            Menu.StartMenu();
         }
     }
 }
